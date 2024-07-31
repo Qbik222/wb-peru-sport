686ae45eaea9e72793dc7dc1ab00b31ddb4c381e
yurii <ignatovyw@gmail.com>
2024-07-31 16:17:57 +0300

    before review

--- front/js/main.js
+++ front/js/main.js
@@ -1,3 +1,8 @@
 window.addEventListener("orientationchange", function() {
     location.reload();
 });
+
+document.querySelector(".dark-btn").addEventListener("click", function(){
+    document.body.classList.toggle("dark")
+    document.body.classList.toggle("light")
+})
