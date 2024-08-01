acf4ebd9d6bd9a31a7a3ec91385adadbf97363f6
yurii <ignatovyw@gmail.com>
2024-08-01 13:08:22 +0300

    test tools removed

--- front/js/main.js
+++ front/js/main.js
@@ -1,8 +1,3 @@
 window.addEventListener("orientationchange", function() {
     location.reload();
 });
-
-document.querySelector(".dark-btn").addEventListener("click", function(){
-    document.body.classList.toggle("dark")
-    document.body.classList.toggle("light")
-})
