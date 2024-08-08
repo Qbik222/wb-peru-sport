f77ea89eecf9a98afa12f7eb65edf7afbfd9aaa9
yurii <ignatovyw@gmail.com>
2024-08-08 09:14:28 +0300

    dark btn add

--- front/js/main.js
+++ front/js/main.js
@@ -1,3 +1,8 @@
 window.addEventListener("orientationchange", function() {
     location.reload();
 });
+
+document.querySelector(".dark-btn").addEventListener("click", () =>{
+    document.body.classList.toggle("light")
+    document.body.classList.toggle("dark")
+})
